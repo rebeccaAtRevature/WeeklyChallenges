083aab516ffec34d74d48d54c4b7013b1eea63bc
rebeccaAtRevature <98355913+rebeccaAtRevature@users.noreply.github.com>
2022-05-05 20:46:22 -0400

    removed unfinished iterator, turned out unnecessary

--- src/main/java/CorruptableLinkedList.java
+++ src/main/java/CorruptableLinkedList.java
@@ -1,6 +1,5 @@
-import java.util.Iterator;
 
-public class CorruptableLinkedList<T> implements Iterable<T> {
+public class CorruptableLinkedList<T> {
 	
 	// Code taken from : https://www.geeksforgeeks.org/linked-list-set-1-introduction/
 	Node<T> head;
@@ -8,7 +7,7 @@ public class CorruptableLinkedList<T> implements Iterable<T> {
 	/* Linked List Node */
 	public class Node<T> {
 		int data;
-		Node next;
+		Node<T> next;
 		
 		// Constructor to create a new node
 		// Next is by default initialized as null
@@ -16,12 +15,6 @@ public class CorruptableLinkedList<T> implements Iterable<T> {
 			data = d;
 			next = null;
 		}
-	}
-
-	@Override
-	public Iterator<T> iterator() {
-		
-		return new CorruptLinkedListIterator();
 	}
 
 }
